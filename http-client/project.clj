913ae93edc6717a4eb28673e1b4f4dc593210675
mikub <miroslav.kubicek@gmail.com>
2019-01-30 08:44:37 +0100

    initial commit

---
+++ http-client/project.clj
@@ -0,0 +1,7 @@
+(defproject io.titanoboa.tasklet/http-client "0.1.0"
+  :description "http client step function for titanoboa.io"
+  :url "http://example.com/FIXME"
+  :license {:name "unlicense"
+            :url "http://unlicense.org/"}
+  :dependencies [[clj-http "3.9.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
