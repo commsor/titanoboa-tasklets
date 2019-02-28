5ac64e4977a0bbddcd12acc626c77398b2ce261e
Unknown <miroslav.kubicek@gmail.com>
2019-02-28 20:58:29 +0100

    ssh and sftp tweaks

--- ssh/src/clj/io/titanoboa/tasklet/ssh.clj
+++ ssh/src/clj/io/titanoboa/tasklet/ssh.clj
@@ -2,26 +2,37 @@
   (:require [clj-ssh.ssh :as ssh]))
 
 
-(defn ssh [{:keys [ssh-agent-settings identities host strict-host-key-checking ssh-cmd-map]}]
+(defn ssh [{:keys [ssh-agent-settings identities host session-options ssh-cmd-map]}]
   (let [agent (ssh/ssh-agent (or ssh-agent-settings {}))]
     (when identities
       (cond
         (map? identities) (ssh/add-identity agent identities)
         (vector? identities) (mapv #(ssh/add-identity agent %) identities)
         :else (throw (java.lang.IllegalStateException. "identities can be either a map or a vector"))))
-    (let [session (ssh/session agent host {:strict-host-key-checking strict-host-key-checking})]
+    (let [session (ssh/session agent host session-options)]
       (ssh/with-connection session
                            (ssh/ssh session ssh-cmd-map)))))
 
-(defn sftp [{:keys [ssh-agent-settings identities host strict-host-key-checking sftp-cmds-vec]}]
+(defn- sftp-execute [channel cmd-vec]
+  (apply ssh/sftp channel {} cmd-vec))
+
+(defn- sftp-execute&convert [channel cmd-vec]
+  (if (= :ls (first cmd-vec))
+    (->> (sftp-execute channel cmd-vec)
+         (into [])
+         (mapv (fn[i]
+                 [(.getFilename i) (.getPermissionsString (.getAttrs i)) (.getSize (.getAttrs i)) (.getMtimeString(.getAttrs i)) (.getMTime (.getAttrs i))])))
+    (sftp-execute channel cmd-vec)))
+
+(defn sftp [{:keys [ssh-agent-settings identities host session-options sftp-cmds-vec]}]
   (let [agent (ssh/ssh-agent (or ssh-agent-settings {}))]
     (when identities
       (cond
         (map? identities) (ssh/add-identity agent identities)
         (vector? identities) (mapv #(ssh/add-identity agent %) identities)
         :else (throw (java.lang.IllegalStateException. "identities can be either a map or a vector"))))
-    (let [session (ssh/session agent host {:strict-host-key-checking strict-host-key-checking})]
+    (let [session (ssh/session agent host session-options)]
       (ssh/with-connection session
                            (let [channel (ssh/ssh-sftp session)]
                              (ssh/with-channel-connection channel
-                                                          (mapv #(ssh/sftp channel {} (first %) (second %)) sftp-cmds-vec)))))))
+                                                          {:sftp-out (mapv #(sftp-execute&convert channel %) sftp-cmds-vec)}))))))
