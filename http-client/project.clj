355d924f623d616c3049a3ac8890d5c616fa1564
mikub <miroslav.kubicek@gmail.com>
2022-01-01 22:09:37 +0100

    dependencies version bump

--- http-client/project.clj
+++ http-client/project.clj
@@ -3,7 +3,7 @@
   :url "https://titanoboa.io"
   :license {:name "unlicense"
             :url "http://unlicense.org/"}
-  :dependencies [[clj-http "3.9.1"]]
+  :dependencies [[clj-http "3.12.3"]]
   :source-paths ["src/clj"]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}
              :provided {:dependencies [[org.clojure/clojure "1.10.0"]]}})
