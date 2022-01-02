7916df7ec368cdd8b40b670cf3f3957d536ac3e6
mikub <miroslav.kubicek@gmail.com>
2022-01-02 18:27:26 +0100

    slack tasklet: add return-code

--- slack/src/clj/io/titanoboa/tasklet/slack.clj
+++ slack/src/clj/io/titanoboa/tasklet/slack.clj
@@ -2,12 +2,12 @@
   (:require [clj-http.client :as client]))
 
 (defn get-channel-info [{:keys [token channel response-property-name] :as properties}]
-  "Returns channel's information for given channel id"
+  "Returns channel's information for given Slack channel id"
   (let [response (client/request {:url "https://slack.com/api/conversations.info"
                                   :request-method :get
                                   :as :json
                                   :content-type :x-www-form-urlencoded
                                   :query-params {"channel" channel}
                                   :headers {"Authorization" (str "Bearer " token)}})]
-    {:properties
-     {response-property-name (:body response)}}))
+    {:properties {response-property-name (:body response)}
+     :return-code (get-in response [:body :ok])}))
