206a5c0783841b0b438738a89c5e21a7aac6414b
mikub <miroslav.kubicek@gmail.com>
2022-01-03 16:15:25 +0100

    slack tasklet: add sample deploy-repositories into project.clj

--- slack/project.clj
+++ slack/project.clj
@@ -1,5 +1,5 @@
 (defproject io.titanoboa.tasklet/slack "0.0.1"
-  :description "slack client step functions for titanoboa.io"
+  :description "slack client tasklets for titanoboa.io"
   :url "https://titanoboa.io"
   :license {:name "unlicense"
             :url "http://unlicense.org/"}
@@ -10,4 +10,7 @@
                                   [cheshire "5.9.0"]]}
              :provided {:dependencies [[org.clojure/clojure "1.10.0"]
                                        [clj-http "3.12.3"]
-                                       [cheshire "5.9.0"]]}})
+                                       [cheshire "5.9.0"]]}}
+;;  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org/repo"}]
+;;                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
+)
