12426a33024e412de339d354d4f02bb0fc31b50e
mikub <miroslav.kubicek@gmail.com>
2022-01-03 12:38:34 +0100

    slack tasklet: add post-message fn

--- slack/src/clj/io/titanoboa/tasklet/slack.clj
+++ slack/src/clj/io/titanoboa/tasklet/slack.clj
@@ -18,10 +18,26 @@
   :return-code in the properties map is true if the call was successful, false otherwise."
   (let [response (client/request {:url "https://slack.com/api/chat.getPermalink"
                                   :request-method :get
-                                  :as :json
                                   :content-type :x-www-form-urlencoded
+                                  :as :json
                                   :query-params {"channel" channel
                                                  "message_ts" message_ts}
+                                  :headers {"Authorization" (str "Bearer " token)}})]
+    {:properties {response-property-name (:body response)}
+     :return-code (get-in response [:body :ok])}))
+
+(defn post-message [{:keys [token channel blocks as_user response-property-name thread_ts] :as properties}]
+  "Sends a Slack message (using :blocks) to given channel.
+  If the thread_ts is provided, the message will be a response to given parent thread.
+  :return-code in the properties map is true if the call was successful, false otherwise."
+  (let [default-params (cond-> {}
+                         (some? as_user) (assoc :as_user as_user)
+                         (some? thread_ts) (assoc :thread_ts thread_ts))
+        response (client/request {:url "https://slack.com/api/chat.postMessage"
+                                  :request-method :post
+                                  :content-type :x-www-form-urlencoded
+                                  :as :json
+                                  :form-params (merge default-params {:channel channel :blocks blocks})
                                   :headers {"Authorization" (str "Bearer " token)}})]
     {:properties {response-property-name (:body response)}
      :return-code (get-in response [:body :ok])}))
